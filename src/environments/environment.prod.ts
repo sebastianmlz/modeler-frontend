a22785638526b2dd46b0273b4a6b4efdb2c6ba37
sebastianmlz_07 <sebamendex11@gmail.com>
2025-09-28 13:42:33 -0400

    feat: simplify to traditional environment approach - remove complex set-env script

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://modeler-backend-production.up.railway.app',
+  geminiApiKey: 'AIzaSyBxxxxxxx' // Placeholder - Railway inyectará la real
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8000',
+  geminiApiKey: 'AIzaSyBxxxxxxx' // Para desarrollo local - poner key de desarrollo
+};
